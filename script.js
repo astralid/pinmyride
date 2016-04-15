2f6d72eae819344551d78b252fffc70bc9cd97c3
astralid <astralid@users.noreply.github.com>
2016-04-15 03:22:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,16 +13,22 @@
     }
     var nfo = document.getElementById('nfo');
     nfo.innerHTML = JSON.stringify(configuration);
-
-    var fields = document.getElementsByTagName('input');
-    var length = fields.length;
-    while (length--) {
-        var field = fields[length];
-        setValue.call(field);
-        field.addEventListener('change', setValue, false);
+    var stopsElem = document.getElementById('stops');
+    var def_input = stopsElem.getElementsByTagName('input')[0];
+    
+    var i, l = configuration.stops.length;
+    for (i = 0; i < l; ++i) {
+        var inp = def_input.cloneNode();
+        inp.ind = i;
+        inp.value = configuration.stops[i];
+        stopsElem.insertBefore(inp, stopsElem.firstChild);
+        inp.addEventListener('change', setValue, false);
     }
+    def_input.ind = l;
+    def_input.addEventListener('change', setValue, false);
+    
     function setValue() {
-        configuration[this.getAttribute('data-target')] = this.value;
+        configuration.stops[this.getAttribute('ind')] = this.value;// == '' ? null : this.value;
     }
 
     document.getElementById('save').onclick = function() {
